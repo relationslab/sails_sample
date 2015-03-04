c8848dc05e70954749f2a58e748d8a5105962e98
takayukii <takayuki_imanishi@relationsgroup.co.jp>
2015-03-04 15:42:43 +0900

    sample.mwbを追加

---
+++ resources/sample.sql
@@ -0,0 +1,45 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema sample
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `sample` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `sample` ;
+
+-- -----------------------------------------------------
+-- Table `sample`.`articles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sample`.`articles` (
+  `id` INT NOT NULL,
+  `title` VARCHAR(45) NULL,
+  `body` VARCHAR(45) NULL,
+  `createdAt` DATETIME NULL,
+  `updatedAt` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sample`.`tags`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sample`.`tags` (
+  `id` INT NOT NULL,
+  `article_id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `createdAt` DATETIME NULL,
+  `updatedAt` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_tags_articles_idx` (`article_id` ASC),
+  CONSTRAINT `fk_tags_articles`
+    FOREIGN KEY (`article_id`)
+    REFERENCES `sample`.`articles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
