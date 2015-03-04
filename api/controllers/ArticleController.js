33f1438c37bb1bf75d174c743e133e0c032db1ad
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2015-03-04 21:51:08 +0900

    api/view controller

---
+++ api/controllers/ArticleController.js
@@ -0,0 +1,17 @@
+module.exports = {
+  find: function(req, res) {
+    var page = req.param('page');
+
+    if (!page) {
+      return res.send(400);
+    }
+
+    sails.controllers['api/article']._findArticles({page: page}, function(error, articles) {
+      if (error) {
+        return res.send(500);
+      }
+
+      res.view('homepage', {articles: articles});
+    });
+  }
+};
