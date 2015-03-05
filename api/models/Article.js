afd9b7e60f3d3aea26b1ea5311d4d5d10b3b9087
takayukii <takayuki_imanishi@relationsgroup.co.jp>
2015-03-05 14:35:40 +0900

    DBに繋がるところまで

---
+++ api/models/Article.js
@@ -0,0 +1,16 @@
+/**
+* Article.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  tableName: 'articles',
+  attributes: {
+    title: 'STRING',
+    body: 'STRING'
+  }
+};
+
--- resources/sample.sql
+++ resources/sample.sql
@@ -12,9 +12,9 @@ USE `sample` ;
 -- Table `sample`.`articles`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sample`.`articles` (
-  `id` INT NOT NULL,
-  `title` VARCHAR(45) NULL,
-  `body` VARCHAR(45) NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' NULL,
+  `body` VARCHAR(255) NULL,
   `createdAt` DATETIME NULL,
   `updatedAt` DATETIME NULL,
   PRIMARY KEY (`id`))
@@ -25,14 +25,33 @@ ENGINE = InnoDB;
 -- Table `sample`.`tags`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sample`.`tags` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `article_id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
+  `name` VARCHAR(255) NULL,
   `createdAt` DATETIME NULL,
   `updatedAt` DATETIME NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_tags_articles_idx` (`article_id` ASC),
   CONSTRAINT `fk_tags_articles`
+    FOREIGN KEY (`article_id`)
+    REFERENCES `sample`.`articles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sample`.`comments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sample`.`comments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `article_id` INT NOT NULL,
+  `body` VARCHAR(255) NULL,
+  `createdAt` DATETIME NULL,
+  `updatedAt` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_tags_articles_idx` (`article_id` ASC),
+  CONSTRAINT `fk_tags_articles0`
     FOREIGN KEY (`article_id`)
     REFERENCES `sample`.`articles` (`id`)
     ON DELETE NO ACTION
