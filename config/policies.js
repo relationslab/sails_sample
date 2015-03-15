2baeac3629590c08e4d19699303e9be8cc62088f
久原政彦 <masahiko_kubara@relationsgroup.co.jp>
2015-03-16 00:55:57 +0900

    fix

--- config/policies.js
+++ config/policies.js
@@ -36,7 +36,7 @@ module.exports.policies = {
   ***************************************************************************/
   ArticleController: {
     '*': true
-  },
+  }
 
 	// RabbitController: {
 
